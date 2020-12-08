27f085a58a3c4648608ed80e6d2ce2a77bc95be2
yujian.ou <yujian.ou@upai.com>
2020-12-08 16:13:15 +0800

    fix interceptor log

--- interceptor/log.go
+++ interceptor/log.go
@@ -6,11 +6,19 @@ import (
 )
 
 func logServerInterceptor(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
-	interceptorLogger.Info(info.FullMethod)
+	interceptorLogger.WithFields(
+		map[string]interface{}{
+			"method": info.FullMethod,
+		},
+	).Info()
 	return handler(ctx, req)
 }
 
 func logServerStreamInterceptor(srv interface{}, ss grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler) error {
-	interceptorLogger.Info(info.FullMethod)
+	interceptorLogger.WithFields(
+		map[string]interface{}{
+			"method": info.FullMethod,
+		},
+	).Info()
 	return handler(srv, ss)
 }
